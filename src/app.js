9921e11bae230f2b5cfe180f0ad3ce38cbd901a7
nAgnorT <trongan765@gmail.com>
2024-03-08 19:47:39 +0700

    section-5(1)

--- src/app.js
+++ src/app.js
@@ -16,14 +16,7 @@ require('./dbs/init.mongodb')
 // const { checkOverload } = require('./helpers/check.connect')
 // checkOverload()
 //init routes
-app.get('/',(req,res,next ) => {
-
-    const strCompress = 'Hello World'
-    return res.status(200).json({
-        message:'Welcome Quytjs!',
-        //metadata: strCompress.repeat(10000)
-    })
-})
+app.use('',require('./routes'))
 //handling error
 
 
