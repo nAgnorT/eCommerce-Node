d01a61e6fdc69fa36034a939207f8fba1e717e78
nAgnorT <trongan765@gmail.com>
2024-03-08 14:43:34 +0700

    section 2

---
+++ server.js
@@ -0,0 +1,11 @@
+const app = require("./src/app");
+
+const PORT = 3056
+const server = app.listen(PORT, () =>{
+    console.log(`WSV quytCommerce start with ${PORT}`)
+})
+
+process.on('SIGINT', ()=>{
+    server.close( () => console.log('Exit Server Express'))
+    
+})
