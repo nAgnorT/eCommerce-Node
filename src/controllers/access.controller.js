c0774b62836c3c2eb8125c56b2cdcf4bc91475b2
nAgnorT <trongan765@gmail.com>
2024-03-15 19:49:05 +0700

    section-8

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,11 +1,22 @@
 'use strict'
 
 const AccessService = require('../services/access.service')
+const { CREATED } = require('../core/success.response')
 
 class AccessController{
     signUp = async (req, res, next ) => {
-        
-            return res.status(201).json(await AccessService.signUp(req.body))
+            // return resolveSoa.status(200).json({
+            //     message:'',
+            //     metadata:
+            // }) 
+            new CREATED({
+                message:'Registered OK!',
+                metadata: await AccessService.signUp(req.body),
+                options: {
+                    limit: 10
+                }
+            }).send(res)
+            // return res.status(201).json(await AccessService.signUp(req.body))
     }
 }
 
